2086715a9fd56efc38ca028124855f36422d15b7
011491 <stephe.johnson@delta.com>
2020-01-28 08:50:07 -0500

    API request for likes - begin UI buildout

--- drews-cloud/src/App.js
+++ drews-cloud/src/App.js
@@ -1,21 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { getLikes } from './lib/helpers';
+import styled from 'styled-components';
+import { Layout } from './components/index';
+import { getLikes, mapLikeObjects } from './lib/helpers';
 import { CLIENT_ID, USER_ID } from './lib/constants';
+
+const Likes = styled.div``;
+const Like = styled.div``;
 
 const App = () => {
     const [likes, setLikes] = useState([]);
+    const [nextLikesGetEndpoint, setNextGetLikesEndpoint] = useState('');
+    const [page, setPage] = useState('likes');
+
     useEffect(() => {
-        setLikes(
-            getLikes({
-                userId: USER_ID,
-                clientId: CLIENT_ID,
-                offset: 0,
-                limit: 50
+        getLikes({
+            userId: USER_ID,
+            clientId: CLIENT_ID,
+            offset: 0,
+            limit: 50
+        })
+            .then(({ next_href, collection }) => {
+                setNextGetLikesEndpoint(next_href);
+                setLikes(mapLikeObjects(collection));
             })
-        );
+            .catch(err => console.log(err));
     }, []);
-    console.log(likes);
-    return <div />;
+    console.log('likes', likes);
+    console.log('nextLikesGetEndpoint', nextLikesGetEndpoint);
+    return (
+        <Layout>
+            {page === 'likes' && (
+                <Likes>
+                    {likes.map(({ id }) => (
+                        <Like key={id} />
+                    ))}
+                </Likes>
+            )}
+        </Layout>
+    );
 };
 
 export default App;
--- drews-cloud/src/lib/helpers.js
+++ drews-cloud/src/lib/helpers.js
@@ -1,18 +1,41 @@
-const makeRequest = endpoint => {
-    fetch(endpoint, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: '*/*'
-        }
-    }).then(response => {
-        console.log(response);
+const makeRequest = async endpoint => {
+    const preflightResponse = await fetch(endpoint, {
+        method: 'OPTIONS'
     });
+    if (preflightResponse.status === 200) {
+        const response = await fetch(endpoint, {
+            method: 'GET'
+        });
+        const responseJSON = await response.json();
+        return responseJSON;
+    }
+    return { error: 'Failed Preflight Check' };
 };
 
-const getLikesEndpoint = ({ userId, clientId, limit, offset }) =>
-    `https://api-v2.soundcloud.com/users/${userId}/likes?client_id=${clientId}&offset=${offset}&limit=${limit}`;
+const getLikesEndpoint = ({
+    userId,
+    clientId,
+    limit,
+    offset,
+    nextLikesGetEndpoint
+}) =>
+    nextLikesGetEndpoint
+        ? nextLikesGetEndpoint
+        : `https://api-v2.soundcloud.com/users/${userId}/likes?client_id=${clientId}&offset=${offset}&limit=${limit}`;
 
 export const getLikes = ({ userId, clientId, limit, offset }) =>
     makeRequest(getLikesEndpoint({ userId, clientId, limit, offset }));
+
+const LIKE_FILEDS = [
+    { id: 'artwork_url', label: 'image' },
+    { id: 'duration', label: 'duration' },
+    { id: 'id', label: 'id' },
+    { id: 'title', label: 'title' },
+    { id: 'user', label: 'user' }
+];
+export const mapLikeObjects = likes =>
+    likes.map(({ created_at, track }) => {
+        const newLikeObj = { created: created_at };
+        LIKE_FILEDS.forEach(({ id, label }) => (newLikeObj[label] = track[id]));
+        return newLikeObj;
+    });
